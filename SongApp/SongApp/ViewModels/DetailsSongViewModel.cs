da21e0147604477dd9d448bddc6f0838dc2c265b
fmassaretto <fmassaretto@WKSJUN000494>
2017-05-04 14:44:19 -0300

    Add DetailPage

---
+++ SongApp/SongApp/ViewModels/DetailsSongViewModel.cs
@@ -0,0 +1,69 @@
+﻿using SongApp.Models;
+using SongApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongApp.ViewModels
+{
+    public class DetailsSongViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Song currentSong;
+        public DetailsSongViewModel(Song song) : base("Song Details")
+        {
+            this.currentSong = song;
+        }
+
+        public string Album
+        {
+            get { return currentSong.Album; }
+
+        }
+
+        public string Artist
+        {
+            get { return currentSong.Artist; }
+        }
+
+        public int BitRate
+        {
+            get { return currentSong.BitRate; }
+        }
+
+        public string Composer
+        {
+            get { return currentSong.Composer; }
+
+        }
+
+        public string Genre
+        {
+            get { return currentSong.Genre; }
+        }
+
+        public string Name
+        {
+            get { return currentSong.Name; }
+        }
+
+        public long TotalTime
+        {
+            get { return currentSong.TotalTime; }
+        }
+
+        public int TrackNumber
+        {
+            get { return currentSong.TrackNumber; }
+        }
+
+        public int Year
+        {
+            get { return currentSong.Year; }
+        }
+    }
+}
---
+++ SongApp/SongApp/Views/DetailsSongPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SongApp.Models;
+using SongApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SongApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsSongPage : ContentPage
+    {
+        public DetailsSongPage(Song song)
+        {
+            InitializeComponent();
+            BindingContext = new DetailsSongViewModel(song);
+        }
+    }
+}
